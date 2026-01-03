b52d2a5082fc955275f7fa29a0506308ff3120da
Yohan <yohan.develop@gmail.com>
2026-01-03 17:33:28 +0000

    Documenting the database contract

--- core_sdk/repository/lib/src/contracts/database_contract.dart
+++ core_sdk/repository/lib/src/contracts/database_contract.dart
@@ -1,14 +1,19 @@
 import 'package:repository/src/parameters/filter_parameters.dart';
 
 abstract interface class DatabaseContract {
+  /// Use this function to get a list of data from remote database, is necessary inform the table some filter.
   Future<List<Map<String, dynamic>>> readData(
     String tableName, {
     required FilterParameters filter,
   });
+
+  /// Use this function to save some new data, passing the table name and the information that will be save on the database.
+  /// Attention, the table must be already existent.
   Future<void> insertData(
     String tableName, {
     required Map<String, dynamic> data,
   });
+
   Future<void> updateData(
     String tableName, {
     required Map<String, dynamic> data,
