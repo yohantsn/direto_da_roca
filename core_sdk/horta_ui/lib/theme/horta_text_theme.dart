03b3eab03bee65824c0921e8489424ce516ab4e8
Yohan <yohan.develop@gmail.com>
2026-04-28 22:05:45 +0100

    Adding the new UI specs

--- core_sdk/horta_ui/lib/theme/horta_text_theme.dart
+++ core_sdk/horta_ui/lib/theme/horta_text_theme.dart
@@ -4,16 +4,41 @@ import 'package:google_fonts/google_fonts.dart';
 class HortaTextTheme extends TextTheme {
   HortaTextTheme.light()
     : super(
-        displayLarge: GoogleFonts.playfairDisplay(
-          color: Color(0xFF4A2C2A),
-          fontWeight: .bold,
+        headlineLarge: GoogleFonts.epilogue(
+          color: Colors.black87,
+          fontSize: 24,
+          fontWeight: .w800,
           height: 1.5,
         ),
-        headlineMedium: GoogleFonts.montserrat(
+        headlineSmall: GoogleFonts.epilogue(
+          color: Colors.black87,
+          fontSize: 20,
+          fontWeight: .w800,
+          height: 1.5,
+        ),
+        bodySmall: GoogleFonts.epilogue(
+          color: Color(0xFF4E342E),
+          fontSize: 14,
+          fontWeight: .w500,
+          height: 1.5,
+        ),
+        bodyLarge: GoogleFonts.epilogue(
           fontSize: 16,
           fontWeight: .w500,
-          color: Colors.black87,
+          color: Color(0xFF4E342E),
+          height: 1.5,
         ),
-        bodyMedium: GoogleFonts.montserrat(fontSize: 16, color: Colors.black87),
+        labelLarge: GoogleFonts.epilogue(
+          fontSize: 12,
+          color: Color(0xFF4E342E),
+          fontWeight: .w500,
+          height: 1.5,
+        ),
+        labelSmall: GoogleFonts.epilogue(
+          fontSize: 11,
+          color: Color(0xFF4E342E),
+          fontWeight: .w500,
+          height: 1.5,
+        ),
       );
 }
--- core_sdk/horta_ui/lib/theme/horta_theme.dart
+++ core_sdk/horta_ui/lib/theme/horta_theme.dart
@@ -4,6 +4,9 @@ import 'package:horta_ui/theme/horta_text_theme.dart';
 
 class HortaTheme {
   static ThemeData get light {
-    return ThemeData(colorScheme: HortaColor.light(), textTheme: HortaTextTheme.light());
+    return ThemeData(
+      colorScheme: HortaColor.light(),
+      textTheme: HortaTextTheme.light(),
+    );
   }
 }
