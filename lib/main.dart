2a5845713b2a949cee2d6cc7706fa47f49c82b2f
Yohan <yohan.develop@gmail.com>
2026-05-07 22:48:05 +0100

    feat: Simplify main function and UI components in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,45 +5,25 @@ import 'package:horta_ui/horta_ui.dart';
 import 'package:iod/iod.dart';
 
 void main() async {
-  IoD.register<AiuriSDK>(AiuriSDK());
-  final repository = IoD.read<AiuriSDK>();
-  await repository.init(aiuriConfig);
+  // IoD.register<AiuriSDK>(AiuriSDK());
+  // final repository = IoD.read<AiuriSDK>();
+  // await repository.init(aiuriConfig);
   runApp(
     MaterialApp(
       theme: HortaTheme.light,
       home: Builder(
-        builder: (context) => HortaScaffold(
-          appBarDelegate: HortaAppBarDelegate(
-            leadingImagePath: 'assets/images/logo.png',
-
-            webMenuItems: HortaListMenuSectionWeb(
-              menuItems: ['Home', 'Produtos', 'Sobre', 'Contato'],
-              selectedItem: 'Home',
-              onItemPressed: (item) {
-                // Handle menu item press
-              },
+        builder: (context) => Scaffold(
+          appBar: AppBar(title: const Text('Direto da Roca')),
+          body: HortaPadding(
+            padding: .all(.medium),
+            child: Center(
+              child: HortaInputText.email(
+                context,
+                hintText: 'josedasilva@gmail.com',
+                labelText: "Email",
+              ),
             ),
           ),
-          children: [
-            HortaProductGrid(
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return HortaProductCard(
-                  parameters: HortaProductCardParameters(
-                    imageUrl:
-                        "https://www.freeiconspng.com/uploads/no-image-icon.jpg",
-                    title: "Produto $index",
-                    price: (index + 1 * 2.5).toStringAsFixed(2),
-                    unit: "kg",
-                    status: ProductStatus.inStock,
-                    currency: "R\$",
-                    onFailureImage: 'assets/images/logo.png',
-                    onOrderPressed: () {},
-                  ),
-                );
-              },
-            ),
-          ],
         ),
       ),
     ),
