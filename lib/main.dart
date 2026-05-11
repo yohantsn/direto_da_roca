9f1fbc87aa20a2635ede515e6eadc81898efaa4c
Yohan <yohan.develop@gmail.com>
2026-05-11 16:43:29 +0100

    Fixing issues

--- lib/main.dart
+++ lib/main.dart
@@ -5,25 +5,16 @@ import 'package:horta_ui/horta_ui.dart';
 import 'package:iod/iod.dart';
 
 void main() async {
-  // IoD.register<AiuriSDK>(AiuriSDK());
-  // final repository = IoD.read<AiuriSDK>();
-  // await repository.init(aiuriConfig);
+  IoD.register<AiuriSDK>(AiuriSDK());
+  final repository = IoD.read<AiuriSDK>();
+  await repository.init(aiuriConfig);
   runApp(
     MaterialApp(
       theme: HortaTheme.light,
       home: Builder(
         builder: (context) => Scaffold(
           appBar: AppBar(title: const Text('Direto da Roca')),
-          body: HortaPadding(
-            padding: .all(.medium),
-            child: Center(
-              child: HortaInputText.email(
-                context,
-                hintText: 'josedasilva@gmail.com',
-                labelText: "Email",
-              ),
-            ),
-          ),
+          body: HortaPadding(padding: .all(.medium), child: Center()),
         ),
       ),
     ),
